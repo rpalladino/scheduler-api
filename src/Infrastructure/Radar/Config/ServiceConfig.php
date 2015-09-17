a0ad1b2e2b86ddf9417493db9b7a12ea3abf0dd0
Rocco A Palladino <rocky@uchicago.edu>
2015-09-17 15:12:22 -0500

    Add DB_PORT env variable for connecting to a server not running on the default port

--- src/Infrastructure/Radar/Config/ServiceConfig.php
+++ src/Infrastructure/Radar/Config/ServiceConfig.php
@@ -21,6 +21,7 @@ class ServiceConfig extends ContainerConfig
                 'user' => getenv('DB_USER'),
                 'password' => getenv('DB_PASSWORD'),
                 'host' => getenv('DB_HOST'),
+                'port' => getenv('DB_PORT'),
                 'driver' => getenv('DB_DRIVER'),
             ]
         ));
