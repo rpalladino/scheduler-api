e14806ed6acb207476dc1daff309371895ed60b9
Rocco A Palladino <rocky@uchicago.edu>
2015-09-19 11:30:35 -0500

    Add GetEmployee application service

---
+++ spec/Application/Service/GetEmployeeSpec.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace spec\Scheduler\Application\Service;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+use Aura\Payload\Payload;
+use Scheduler\Domain\Model\User\User;
+use Scheduler\Domain\Model\User\UserMapper;
+
+class GetEmployeeSpec extends ObjectBehavior
+{
+    function let(UserMapper $userMapper)
+    {
+        $this->beConstructedWith($userMapper);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Scheduler\Application\Service\GetEmployee');
+    }
+
+    function it_gets_an_employee($userMapper)
+    {
+        $currentUser = new User(1, "John Williamson", "manager", "john@abc.com");
+        $currentUser->authenticate();
+        $employee = new User(3, "Shelly Levene", "employee", "oldguy@aol.com");
+        $userMapper->find(3)->willReturn($employee);
+
+        $payload = $this($currentUser, 3);
+
+        $payload->shouldHaveType(Payload::class);
+        $payload->getStatus()->shouldBe(Payload::SUCCESS);
+        $payload->getOutput()->shouldBe($employee);
+    }
+
+    function it_does_not_allow_unauthenticated_access()
+    {
+        $currentUser = new User(1, "John Williamson", "manager", "john@abc.com");
+
+        $payload = $this($currentUser, 3);
+
+        $payload->getStatus()->shouldBe(Payload::NOT_AUTHENTICATED);
+    }
+}
---
+++ src/Application/Service/GetEmployee.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Scheduler\Application\Service;
+
+use Aura\Payload\Payload;
+use Scheduler\Domain\Model\User\User;
+use Scheduler\Domain\Model\User\UserMapper;
+
+class GetEmployee
+{
+    private $payload;
+    private $userMapper;
+
+    public function __construct(UserMapper $userMapper)
+    {
+        $this->payload = new Payload();
+        $this->userMapper = $userMapper;
+    }
+
+    public function __invoke(User $currentUser, $employeeId)
+    {
+        if (! $currentUser->isAuthenticated()) {
+            return $this->payload->setStatus(Payload::NOT_AUTHENTICATED);
+        }
+
+        $employee = $this->userMapper->find($employeeId);
+
+        return $this->payload->setStatus(Payload::SUCCESS)
+                             ->setOutput($employee);
+    }
+}
